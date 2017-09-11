c74f3dc5167a7824be5cfb455053fc947c009d20
Brandon Rana <31735323+bmrana@users.noreply.github.com>
2017-09-11 08:35:14 -0500

    Before adding comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ClassListComponent } from './class-list/class-list.component';
-import { NameInputComponent } from './name-input/name-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ClassListComponent,
-    NameInputComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
